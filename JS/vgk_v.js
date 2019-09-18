2e07edcf1dde5755663988beefeb2bb92bd70c25
kostt2 <53828504+kostt2@users.noreply.github.com>
2019-09-18 16:53:10 +0300

    masivi

---
+++ JS/vgk_v.js
@@ -0,0 +1,252 @@
+let vgk_c = [
+    [ 'Apvienotie Arābu Emirāti'],  //0
+    [ 'Afganistānas Islāma Republika'],  //1
+    [ 'Albānijas Republika'],  //2
+    [ 'Alžīrijas Tautas Demokrātiskā Republika'],  //3
+    [ 'Andoras Firstiste'],  //4
+    [ 'Angiljas teritorija'],  //5
+    [ 'Angolas Republika'],  //6
+    [ '(kontinents: Antarktīda)'],  //7
+    [ 'Antigva un Barbuda'],  //8
+    [ 'Argentīnas Republika'],  //9
+    [ 'Armēnijas Republika'],  //10
+    [ 'Aruba'],  //11
+    [ 'Amerikas Savienotās Valstis'],  //12
+    [ 'ASV Mazās aizjūras teritorijas'],  //13
+    [ 'ASV Virdžīnu salu teritorija'],  //14
+    [ 'Austrālijas Savienība'],  //15
+    [ 'Austrijas Republika'],  //16
+    [ 'Austrumtimoras Demokrātiskā Republika'],  //17
+    [ 'ASV Samoa teritorija'],  //18
+    [ 'Azerbaidžānas Republika'],  //19
+    [ 'Bahamu Sadraudzība'],  //20
+    [ 'Bahreinas Karaliste'],  //21
+    [ 'Baltkrievijas Republika'],  //22
+    [ 'Bangladešas Tautas Republika'],  //23
+    [ 'Barbadosa'],  //24
+    [ 'Beliza'],  //25
+    [ 'Beļģijas Karaliste'],  //26
+    [ 'Beninas Republika'],  //27
+    [ 'Bermudu salu teritorija'],  //28
+    [ 'Bolīvijas Daudznacionālā Valsts'],  //29
+    [ 'Bosnija un Hercegovina'],  //30
+    [ 'Botsvānas Republika'],  //31
+    [ 'Brazīlijas Federatīvā Republika'],  //32
+    [ 'Britu Virdžīnu salu teritorija'],  //33
+    [ 'Bruneja Darusalama'],  //34
+    [ 'Bulgārijas Republika'],  //35
+    [ 'Burkinafaso'],  //36
+    [ 'Burundijas Republika'],  //37
+    [ 'Butānas Karaliste'],  //38
+    [ 'Buvē sala'],  //39
+    [ 'Centrālāfrikas Republika'],  //40
+    [ 'Čadas Republika'],  //41
+    [ 'Čehijas Republika'],  //42
+    [ 'Čīles Republika'],  //43
+    [ 'Dānijas Karaliste'],  //44
+    [ 'Dienvidāfrikas Republika'],  //45
+    [ 'Dienviddžordžija un Dienvidsendviču salas'],  //46
+    [ 'Korejas Republika'],  //47
+    [ 'Dienvidsudānas Republika'],  //48
+    [ 'Dominikas Sadraudzība'],  //49
+    [ 'Dominikānas Republika'],  //50
+    [ 'Džērsija'],  //51
+    [ 'Džibutijas Republika'],  //52
+    [ 'Ēģiptes Arābu Republika'],  //53
+    [ 'Ekvadoras Republika'],  //54
+    [ 'Ekvatoriālās Gvinejas Republika'],  //55
+    [ 'Eritrejas Valsts'],  //56
+    [ 'Etiopijas Federatīvā Demokrātiskā Republika'],  //57
+    [ 'Folklenda (Malvinu) salu teritorija'],  //58
+    [ 'Fēru salas'],  //59
+    [ 'Fidži Republika'],  //60
+    [ 'Filipīnu Republika'],  //61
+    [ 'Francijas Republika'],  //62
+    [ 'Francijas Dienvidjūru un Antarktikas teritorija'],  //63
+    [ 'Francijas Gviānas departaments'],  //64
+    [ 'Francijas Polinēzijas teritorija'],  //65
+    [ 'Gabonas Republika'],  //66
+    [ 'Gajānas Kooperatīvā Republika'],  //67
+    [ 'Gambijas Republika'],  //68
+    [ 'Ganas Republika'],  //69
+    [ 'Gērnsija'],  //70
+    [ 'Gibraltāra teritorija'],  //71
+    [ 'Grenāda'],  //72
+    [ 'Grenlande'],  //73
+    [ 'Grieķijas Republika'],  //74
+    [ 'Gruzija'],  //75
+    [ 'ASV Guamas teritorija'],  //76
+    [ 'Francijas Gvadelupas departaments'],  //77
+    [ 'Gvatemalas Republika'],  //78
+    [ 'Gvinejas Republika'],  //79
+    [ 'Gvinejas–Bisavas Republika'],  //80
+    [ 'Haiti Republika'],  //81
+    [ 'Hērda salas un Makdonalda salu teritorija'],  //82
+    [ 'Hondurasas Republika'],  //83
+    [ 'Honkongas / Sjanganas īpašais administratīvais rajons (ĶTR)'],  //84
+    [ 'Horvātijas Republika'],  //85
+    [ 'Igaunijas Republika'],  //86
+    [ 'Indijas Republika'],  //87
+    [ 'Indijas Okeāna Britu teritorija'],  //88
+    [ 'Indonēzijas Republika'],  //89
+    [ 'Irākas Republika'],  //90
+    [ 'Irānas Islāma Republika'],  //91
+    [ 'Īrijas Republika'],  //92
+    [ 'Islandes Republika'],  //93
+    [ 'Itālijas Republika'],  //94
+    [ 'Izraēlas Valsts'],  //95
+    [ 'Jamaika'],  //96
+    [ 'Japāna'],  //97
+    [ 'Francijas Jaunkaledonijas teritorija'],  //98
+    [ 'Jaunzēlande'],  //99
+    [ 'Jemenas Republika'],  //100
+    [ 'Jordānijas Hāšimītu Karaliste'],  //101
+    [ 'Kaboverdes Republika'],  //102
+    [ 'Kaimanu salu teritorija'],  //103
+    [ 'Kambodžas Karaliste'],  //104
+    [ 'Kamerūnas Republika'],  //105
+    [ 'Kanāda'],  //106
+    [ 'Bonaire, Sintēstatiusa un Saba'],  //107
+    [ 'Kataras Valsts'],  //108
+    [ 'Kazahstānas Republika'],  //109
+    [ 'Kenijas Republika'],  //110
+    [ 'Kipras Republika'],  //111
+    [ 'Kirasao Valsts'],  //112
+    [ 'Kirgīzu Republika'],  //113
+    [ 'Kiribati Republika'],  //114
+    [ 'Kokosu un Kīlinga salu teritorija'],  //115
+    [ 'Kolumbijas Republika'],  //116
+    [ 'Komoru Savienība'],  //117
+    [ 'Kongo Demokrātiskā Republika'],  //118
+    [ 'Kongo Republika'],  //119
+    [ 'Kosovas Republika'],  //120
+    [ 'Kostarikas Republika'],  //121
+    [ 'Kotdivuāras Republika'],  //122
+    [ 'Krievijas Federācija'],  //123
+    [ 'Kubas Republika'],  //124
+    [ 'Kuka Salas'],  //125
+    [ 'Kuveitas Valsts'],  //126
+    [ 'Ķīnas Tautas Republika (ĶTR)'],  //127
+    [ 'Laosas Tautas Demokrātiskā Republika'],  //128
+    [ 'Latvijas Republika'],  //129
+    [ 'Lesoto Karaliste'],  //130
+    [ 'Libānas Republika'],  //131
+    [ 'Libērijas Republika'],  //132
+    [ 'Lībijas Valsts'],  //133
+    [ 'Lielbritānijas un Ziemeļīrijas Apvienotā Karaliste'],  //134
+    [ 'Lietuvas Republika'],  //135
+    [ 'Lihtenšteinas Firstiste'],  //136
+    [ 'Luksemburgas Lielhercogiste'],  //137
+    [ 'Madagaskaras Republika'],  //138
+    [ 'Francijas Majotas departaments'],  //139
+    [ 'Makao īpašais administratīvais reģions (ĶTR)'],  //140
+    [ 'Malaizija'],  //141
+    [ 'Malāvijas Republika'],  //142
+    [ 'Maldīvijas Republika'],  //143
+    [ 'Mali Republika'],  //144
+    [ 'Maltas Republika'],  //145
+    [ 'Marokas Karaliste'],  //146
+    [ 'Māršala Salu Republika'],  //147
+    [ 'Francijas Martinikas departaments'],  //148
+    [ 'Maurīcijas Republika'],  //149
+    [ 'Mauritānijas Islāma Republika'],  //150
+    [ 'Meksikas Savienotās Valstis'],  //151
+    [ 'Melnkalne'],  //152
+    [ 'Menas sala'],  //153
+    [ 'Mikronēzijas Federatīvās Valstis'],  //154
+    [ 'Mjanmas Savienības Republika'],  //155
+    [ 'Moldovas Republika'],  //156
+    [ 'Monako Firstiste'],  //157
+    [ 'Mongolija'],  //158
+    [ 'Montserratas teritorija'],  //159
+    [ 'Mozambikas Republika'],  //160
+    [ 'Namībijas Republika'],  //161
+    [ 'Nauru Republika'],  //162
+    [ 'Nepālas Demokrātiskā Federatīvā Republika'],  //163
+    [ 'Nīderlande'],  //164
+    [ 'Nigēras Republika'],  //165
+    [ 'Nigērijas Federatīvā Republika'],  //166
+    [ 'Nikaragvas Republika'],  //167
+    [ 'Niue'],  //168
+    [ 'Norfolkas salas teritorija'],  //169
+    [ 'Norvēģijas Karaliste'],  //170
+    [ 'Olandes salas'],  //171
+    [ 'Omānas Sultanāts'],  //172
+    [ 'Pakistānas Islāma Republika'],  //173
+    [ 'Palau Republika'],  //174
+    [ 'Palestīnas Valsts'],  //175
+    [ 'Panamas Republika'],  //176
+    [ 'Papua–Jaungvinejas* Neatkarīgā Valsts'],  //177
+    [ 'Paragvajas Republika'],  //178
+    [ 'Peru Republika'],  //179
+    [ 'Pitkērnas salu teritorija'],  //180
+    [ 'Polijas Republika'],  //181
+    [ 'Portugāles Republika'],  //182
+    [ 'Puertoriko Sadraudzība'],  //183
+    [ 'Francijas Reinjonas departaments'],  //184
+    [ 'Sahāras Arābu Demokrātiskā Republika'],  //185
+    [ 'Ruandas Republika'],  //186
+    [ 'Rumānija'],  //187
+    [ 'Salvadoras Republika'],  //188
+    [ 'Samoa Neatkarīgā Valsts'],  //189
+    [ 'Sanmarīno Republika'],  //190
+    [ 'Santomē un Prinsipi Demokrātiskā Republika'],  //191
+    [ 'Saūda Arābijas Karaliste'],  //192
+    [ 'Seišelu Salu Republika'],  //193
+    [ 'Francijas Senbartelmī teritorija'],  //194
+    [ 'Senegālas Republika'],  //195
+    [ 'Francijas Senmartēnas teritorija'],  //196
+    [ 'Francijas Senpjēras un Mikelonas kopiena'],  //197
+    [ 'Sentkitsas un Nevisas Federācija'],  //198
+    [ 'Sentlūsija'],  //199
+    [ 'Sentvinsenta un Grenadīnas'],  //200
+    [ 'Serbijas Republika'],  //201
+    [ 'Singapūras Republika'],  //202
+    [ 'Sintmārtena'],  //203
+    [ 'Sīrijas Arābu Republika'],  //204
+    [ 'Sjerraleones Republika'],  //205
+    [ 'Slovākijas Republika'],  //206
+    [ 'Slovēnijas Republika'],  //207
+    [ 'Somālijas Federatīvā Republika'],  //208
+    [ 'Somijas Republika'],  //209
+    [ 'Spānijas Karaliste'],  //210
+    [ 'Sudānas Republika'],  //211
+    [ 'Surinamas Republika'],  //212
+    [ 'Svalbāras arhipelāga un Jana Majena salas teritorija'],  //213
+    [ 'Svatini Karaliste'],  //214
+    [ 'Svētās Helēnas salas, Debesbraukšanas salas un Tristana da Kuņas salu teritorija'],  //215
+    [ 'Šrilankas Demokrātiskā Sociālistiskā Republika'],  //216
+    [ 'Šveices Konfederācija'],  //217
+    [ 'Tadžikistānas Republika'],  //218
+    [ 'Ķīnas Republika Taivānā'],  //219
+    [ 'Taizemes Karaliste'],  //220
+    [ 'Tanzānijas Savienotā Republika'],  //221
+    [ 'Tērksas un Kaikosas salu teritorija'],  //222
+    [ 'Togo Republika'],  //223
+    [ 'Tokelau'],  //224
+    [ 'Tongas Karaliste'],  //225
+    [ 'Trinidādas un Tobāgo Republika'],  //226
+    [ 'Tunisijas Republika'],  //227
+    [ 'Turcijas Republika'],  //228
+    [ 'Turkmenistāna'],  //229
+    [ 'Tuvalu'],  //230
+    [ 'Ugandas Republika'],  //231
+    [ 'Ukraina'],  //232
+    [ 'Ungārija'],  //233
+    [ 'Urugvajas Austrumu Republika'],  //234
+    [ 'Uzbekistānas Republika'],  //235
+    [ 'Vācijas Federatīvā Republika (VFR)'],  //236
+    [ 'Vanuatu'],  //237
+    [ 'Vatikāna Pilsētvalsts (Svētais Krēsls)'],  //238
+    [ 'Venecuēlas Bolivāra Republika'],  //239
+    [ 'Vjetnamas Sociālistiskā Republika'],  //240
+    [ 'Volisa un Futunas salu teritorija'],  //241
+    [ 'Zālamana Salas'],  //242
+    [ 'Zambijas Republika'],  //243
+    [ 'Korejas Tautas Demokrātiskā Republika (KTDR)'],  //244
+    [ 'Ziemeļmaķedonijas Republika'],  //245
+    [ 'Ziemeļu Marianas Salu Sadraudzība'],  //246
+    [ 'Ziemsvētku salas teritorija'],  //247
+    [ 'Zimbabves Republika'],  //248
+    [ 'Zviedrijas Karaliste'],  //249    
+    ];
