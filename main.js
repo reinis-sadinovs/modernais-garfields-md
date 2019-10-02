1e7fe749caecdb8c7c54258320affcc21f132ced
reinis-sadinovs <reinis.sadinovs@accenture.com>
2019-10-02 22:06:22 +0300

    Pievienoju funkcionalitati

--- main.js
+++ main.js
@@ -1,53 +1,79 @@
-let gar = 10; //!!Kaut kas nestrādā ar gar!!
-//let gar=vgk.length; //šo aizvākt pēc testēšanas un atdzīvināt vienu virs šīs rindas
+//let gar = 10; //!!Kaut kas nestrādā ar gar!!
+let gar=vgk.length; //šo aizvākt pēc testēšanas un atdzīvināt vienu virs šīs rindas
 let nr = []; //nodefinē viendimensiju masīvu
 for (let i = 0; i < 7; i++) {
    nr[i] = []; //nodefinē masīvam otru dimensiju; nr[i][0] - glabā jautājuma numuru un tur pat arī pareizo atbildi
 }
 
+function jaunaSpele() {
+    //this.inicializet();
+    //console.log("Sākam jaunu spēli!");
+    const g = new Galvaspilsetas;
+    g.inicializet();
+}
+
 class Galvaspilsetas{
-    constructor(id) {
-        this.konteiners = document.getElementById(id);
-        if (this.konteiners) {
-        this.divPareizi = document.createElement("div");
-        this.divPareizi.setAttribute("class", "pareizi");
-        this.konteiners.appendChild(this.divPareizi);
-
-        this.divJautajums=document.createElement("div");
-        this.divJautajums.setAttribute("id","jautajums");
-        this.divJautajums.innerHTML="JAUTĀJUMS";
-        this.konteiners.appendChild(this.divJautajums);
-                
-        this.divAtbilzuVar=document.createElement("div");
-        this.divAtbilzuVar.setAttribute("id","atbilzu_var");
-        this.konteiners.appendChild(this.divAtbilzuVar);
-
-        let atbildes=[];
-
-        for(let i=0;i<6;i++){
-            let str1="Atbildes ";
-            atbildes[i]=str1.concat(i,"<br>");
-            this.radioVar= document.createElement("INPUT");
-            this.radioVar.setAttribute("type", "radio");
-            this.radioVar.setAttribute("name", "r1");
-            this.radioVar.setAttribute("value", i);
-            this.divAtbilzuVar.appendChild(this.radioVar);
-            this.radioVar.innerHTML=atbildes[i];
-            //this.radioVar.innerHTML="Atbildes<br>";
-        }
-
-        }
+    constructor() {
+        this.izvele = null;
         //this.statuss=Galvaspilsetas.STATUSS_NEINICIALIZETS;
 
     }
 
-    jaunaSpele() {
-        // console.log("Sākam jaunu spēli!");
-      }
+    inicializet() {
+        this.konteiners = document.getElementById("konteiners");
+        if (this.konteiners) {
+            this.divPareizi = document.createElement("div");
+            this.divPareizi.setAttribute("class", "pareizi");
+            this.konteiners.appendChild(this.divPareizi);
+
+            this.divJautajums=document.createElement("div");
+            this.divJautajums.setAttribute("id","jautajums");
+            this.divJautajums.innerHTML="JAUTĀJUMS";
+            this.konteiners.appendChild(this.divJautajums);
+                    
+            this.divAtbilzuVar=document.createElement("div");
+            this.divAtbilzuVar.setAttribute("id","atbilzu_var");
+            this.konteiners.appendChild(this.divAtbilzuVar);
+
+            let atbildes=[];
+
+            for(let i=0;i<6;i++){
+                let str1="Atbildes ";
+                atbildes[i]=str1.concat(i,"<br>");
+                this.radioVar= document.createElement("INPUT");
+                this.radioLabel = document.createElement("LABEL");
+                this.radioVar.setAttribute("type", "radio");
+                this.radioVar.setAttribute("name", "r1");
+                this.radioVar.setAttribute("value", i);
+                this.radioVar.setAttribute("id", `radio${i}`);
+                this.radioVar.onchange = () => this.izvele = i;
+                this.radioLabel.setAttribute("for", `radio${i}`);
+                this.radioLabel.innerHTML=atbildes[i];
+                this.divAtbilzuVar.appendChild(this.radioVar);
+                this.divAtbilzuVar.appendChild(this.radioLabel);
+                
+                //this.radioVar.innerHTML="Atbildes<br>";
+            }
+
+            this.atbildet = document.createElement("BUTTON");
+            this.atbildet.innerHTML = 'Atbildet';
+            this.atbildet.onclick = () => this.atbildets();
+            this.divAtbilzuVar.appendChild(this.atbildet);
+
+        }
+    }
 
     jaunsJautajums() {
 
-      }    
+    }  
+
+    atbildets() {
+        console.log(this.izvele);
+    }
+    
+    // izvele(i) {
+    //     console.log(i);
+    // }
 
   
 
