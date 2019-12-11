fc9f2f4456db37dffed32543beec1df022114f9e
artursausejs <54137029+artursausejs@users.noreply.github.com>
2019-12-11 16:10:37 +0200

    izmaiņas html struktūrā

---
+++ login.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, redirect, url_for, request
+app = Flask(__name__)
+# Route for handling the login page logic
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return render_template('index.html')
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    return render_template('/', error=error)
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        uname = request.form['uname']
+        mail = request.form['mail']
+        passw = request.form['passw']
+
+        register = user(username = uname, email = mail, password = passw)
+        db.session.add(register)
+        db.session.commit()
+
+        return redirect(url_for("/"))
+    return render_template("register.html")
+
