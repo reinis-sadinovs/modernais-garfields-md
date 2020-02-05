08490b25f4208712b8fbf11bf52277d5c42394e0
tatjanalukjanova <49993367+tatjanalukjanova@users.noreply.github.com>
2020-02-05 22:11:46 +0200

    post un get funkcijas

--- main.js
+++ main.js
@@ -385,3 +385,29 @@ xhrs.setRequestHeader("Content-type", "application/json, charset=utf-8");
 xhrs.send(JSON.stringify(sttdata));
   
 }
+
+@app.route('/chats/lasi')
+def ielasit_chatu():
+  chata_rindas=[]
+  with open("chats.txt","r",encoding="UTF-8") as f:
+     for rinda in f:
+       chata_rindas.append(rinda)
+  return jsonify({"chats":chata_rindas})
+
+
+@app.route('/chats/suuti',methods=['POST'])
+def suuti_zinju():
+  print('sanemu datus')
+  dati=request.json
+  print(dati)
+  with open("chats.txt", "a", newline="") as f:
+    f.write(dati["chats"] + "\n")
+
+  return ielasit_chatu()
+
+
+
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000, debug=True)
